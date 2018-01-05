29c471662488fbb590f7ed9ee6f0b01f1e7a7cab
psi <abc@abc.com>
2018-01-05 17:49:02 +0900

    winform 디자인수정

--- PosProject_psi/PosProject_psi/PosProject_psi/PosProject_psi/OrderInventoryForm.Designer.cs
+++ PosProject_psi/PosProject_psi/PosProject_psi/PosProject_psi/OrderInventoryForm.Designer.cs
@@ -28,8 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
@@ -43,6 +43,12 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.lblDate = new System.Windows.Forms.Label();
+            this.listBag = new System.Windows.Forms.ListBox();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.cboType = new System.Windows.Forms.ComboBox();
+            this.btnSeacrch = new System.Windows.Forms.Button();
+            this.btnOrder = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
             this.tabPage2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.itemListGrid)).BeginInit();
@@ -57,7 +63,7 @@
             this.tabControl1.Location = new System.Drawing.Point(0, 0);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(786, 528);
+            this.tabControl1.Size = new System.Drawing.Size(987, 602);
             this.tabControl1.TabIndex = 0;
             // 
             // tabPage1
@@ -66,13 +72,19 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(778, 502);
+            this.tabPage1.Size = new System.Drawing.Size(979, 576);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "재고";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
             // tabPage2
             // 
+            this.tabPage2.Controls.Add(this.btnCancel);
+            this.tabPage2.Controls.Add(this.btnOrder);
+            this.tabPage2.Controls.Add(this.btnSeacrch);
+            this.tabPage2.Controls.Add(this.cboType);
+            this.tabPage2.Controls.Add(this.txtSearch);
+            this.tabPage2.Controls.Add(this.listBag);
             this.tabPage2.Controls.Add(this.label6);
             this.tabPage2.Controls.Add(this.itemListGrid);
             this.tabPage2.Controls.Add(this.itemGridView);
@@ -82,7 +94,7 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(778, 502);
+            this.tabPage2.Size = new System.Drawing.Size(979, 576);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "발주";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -91,7 +103,7 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("휴먼매직체", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label6.Location = new System.Drawing.Point(10, 48);
+            this.label6.Location = new System.Drawing.Point(10, 67);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(61, 16);
             this.label6.TabIndex = 15;
@@ -100,49 +112,49 @@
             // itemListGrid
             // 
             this.itemListGrid.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
-            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
-            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle1.Font = new System.Drawing.Font("돋움", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.itemListGrid.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("돋움", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.itemListGrid.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle3;
             this.itemListGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.itemListGrid.ImeMode = System.Windows.Forms.ImeMode.Off;
-            this.itemListGrid.Location = new System.Drawing.Point(13, 67);
+            this.itemListGrid.Location = new System.Drawing.Point(13, 86);
             this.itemListGrid.Name = "itemListGrid";
             this.itemListGrid.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.itemListGrid.RowTemplate.Height = 23;
-            this.itemListGrid.Size = new System.Drawing.Size(746, 155);
+            this.itemListGrid.Size = new System.Drawing.Size(634, 155);
             this.itemListGrid.TabIndex = 14;
             // 
             // itemGridView
             // 
             this.itemGridView.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
-            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
-            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle2.Font = new System.Drawing.Font("돋움", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.itemGridView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("돋움", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.itemGridView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle4;
             this.itemGridView.ColumnHeadersHeight = 18;
             this.itemGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
             this.itemGridView.ImeMode = System.Windows.Forms.ImeMode.Off;
-            this.itemGridView.Location = new System.Drawing.Point(13, 252);
+            this.itemGridView.Location = new System.Drawing.Point(13, 334);
             this.itemGridView.Name = "itemGridView";
             this.itemGridView.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.itemGridView.RowTemplate.Height = 23;
-            this.itemGridView.Size = new System.Drawing.Size(746, 200);
+            this.itemGridView.Size = new System.Drawing.Size(634, 169);
             this.itemGridView.TabIndex = 13;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("휴먼매직체", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label5.Location = new System.Drawing.Point(10, 233);
+            this.label5.Location = new System.Drawing.Point(10, 315);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(61, 16);
             this.label5.TabIndex = 12;
@@ -159,7 +171,7 @@
             this.panel1.Controls.Add(this.lblDate);
             this.panel1.Location = new System.Drawing.Point(13, 6);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(746, 36);
+            this.panel1.Size = new System.Drawing.Size(954, 36);
             this.panel1.TabIndex = 9;
             // 
             // label4
@@ -214,11 +226,83 @@
             this.lblDate.TabIndex = 1;
             this.lblDate.Text = "날짜";
             // 
+            // listBag
+            // 
+            this.listBag.FormattingEnabled = true;
+            this.listBag.ItemHeight = 16;
+            this.listBag.Location = new System.Drawing.Point(653, 83);
+            this.listBag.Name = "listBag";
+            this.listBag.Size = new System.Drawing.Size(314, 420);
+            this.listBag.TabIndex = 16;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(134, 247);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(193, 26);
+            this.txtSearch.TabIndex = 17;
+            this.txtSearch.Text = "검색어";
+            // 
+            // cboType
+            // 
+            this.cboType.FormattingEnabled = true;
+            this.cboType.Items.AddRange(new object[] {
+            "상품명",
+            "바코드"});
+            this.cboType.Location = new System.Drawing.Point(13, 249);
+            this.cboType.Name = "cboType";
+            this.cboType.Size = new System.Drawing.Size(100, 24);
+            this.cboType.TabIndex = 18;
+            this.cboType.Text = "검색항목";
+            // 
+            // btnSeacrch
+            // 
+            this.btnSeacrch.BackColor = System.Drawing.Color.Orange;
+            this.btnSeacrch.BackgroundImage = global::PosProject_psi.Properties.Resources.if_search_298865;
+            this.btnSeacrch.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnSeacrch.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnSeacrch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSeacrch.Font = new System.Drawing.Font("굴림", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnSeacrch.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnSeacrch.Location = new System.Drawing.Point(327, 247);
+            this.btnSeacrch.Name = "btnSeacrch";
+            this.btnSeacrch.Size = new System.Drawing.Size(27, 27);
+            this.btnSeacrch.TabIndex = 19;
+            this.btnSeacrch.UseVisualStyleBackColor = false;
+            // 
+            // btnOrder
+            // 
+            this.btnOrder.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btnOrder.FlatAppearance.BorderColor = System.Drawing.Color.MintCream;
+            this.btnOrder.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOrder.Font = new System.Drawing.Font("굴림", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnOrder.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnOrder.Location = new System.Drawing.Point(857, 509);
+            this.btnOrder.Name = "btnOrder";
+            this.btnOrder.Size = new System.Drawing.Size(110, 52);
+            this.btnOrder.TabIndex = 20;
+            this.btnOrder.Text = "주문";
+            this.btnOrder.UseVisualStyleBackColor = false;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btnCancel.FlatAppearance.BorderColor = System.Drawing.Color.MintCream;
+            this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancel.Font = new System.Drawing.Font("굴림", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnCancel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnCancel.Location = new System.Drawing.Point(741, 509);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(110, 52);
+            this.btnCancel.TabIndex = 20;
+            this.btnCancel.Text = "취소";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            // 
             // OrderInventoryForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(783, 525);
+            this.ClientSize = new System.Drawing.Size(983, 596);
             this.Controls.Add(this.tabControl1);
             this.Name = "OrderInventoryForm";
             this.Text = "OrderInventoryForm";
@@ -249,5 +333,11 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.DataGridView itemListGrid;
+        private System.Windows.Forms.ComboBox cboType;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.ListBox listBag;
+        private System.Windows.Forms.Button btnSeacrch;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnOrder;
     }
 }
