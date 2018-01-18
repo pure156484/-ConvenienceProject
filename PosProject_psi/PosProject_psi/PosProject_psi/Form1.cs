97c30f6ce547f2607a111ea21bdea2483356f68e
이보완 <abc@abc.com>
2018-01-18 15:16:40 +0900

    충돌해결

--- PosProject_psi/PosProject_psi/PosProject_psi/Form1.cs
+++ PosProject_psi/PosProject_psi/PosProject_psi/Form1.cs
@@ -15,16 +15,12 @@ namespace PosProject_psi
 {
     public partial class MainForm : Form
     {
-        SellAge sa = new SellAge();
-        int eventPrice;
-        CardPay cp;
         SqlDataAdapter adapter;
         DataSet ds;
         SqlDataAdapter picAdapter;
         DataSet picDs;
         SqlConnection sqlcon = null;
         SqlConnection picSqlcon = null;
-        DataGridViewRow EventDr;
         int eventNum;
         int noCount = 1;
         string prodCount;
@@ -43,9 +39,6 @@ namespace PosProject_psi
 
         private void MainForm_Load(object sender, EventArgs e)
         {
-            sa.StartPosition = this.StartPosition;
-            sa.TopMost = true;
-            //this.Enabled = false;
             itemGrid.ColumnCount = 6;
             itemGrid.Columns[0].Name = "NO";
             itemGrid.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
@@ -54,32 +47,10 @@ namespace PosProject_psi
             itemGrid.Columns[3].Name = "가 격";
             itemGrid.Columns[4].Name = "수 량";
             itemGrid.Columns[5].Name = "행 사";
-            sa.Show();
-            sa.btn13w.Click += BtnAge_Click;
-            sa.btn19w.Click += BtnAge_Click;
-            sa.btn20w.Click += BtnAge_Click;
-            sa.btn30w.Click += BtnAge_Click;
-            sa.btn50w.Click += BtnAge_Click;
-            sa.btn13m.Click += BtnAge_Click;
-            sa.btn19m.Click += BtnAge_Click;
-            sa.btn20m.Click += BtnAge_Click;
-            sa.btn30m.Click += BtnAge_Click;
-            sa.btn50m.Click += BtnAge_Click;
-        }
-
-        private void BtnAge_Click(object sender, EventArgs e)
-        {
-            Button btn = (Button)sender;
-            string btnStr = btn.Text.ToString().Trim().Replace(" ", "");
-            int serachBtn = btn.Text.ToString().Trim().Replace(" ", "").IndexOf('(', 0);
-            this.lblGender.Text = btnStr.Substring(serachBtn, 3).ToString();
-            this.lblAge.Text = btnStr.Substring(0, serachBtn);
-            sa.Close();
         }
 
         private void button6_Click(object sender, EventArgs e)
         {
-            //this.Close();
             new GarbageProd().Show();
         }
 
@@ -131,7 +102,6 @@ namespace PosProject_psi
         private void btnOrder_Click_1(object sender, EventArgs e)
         {
             new OrderInventoryForm().Show();
-            this.Hide();
         }
 
         private void txtBacode_KeyDown(object sender, KeyEventArgs e)
@@ -139,7 +109,6 @@ namespace PosProject_psi
             if( e.KeyCode == Keys.Enter)
             {
                 InputInfo();
-                BonusEvent();
             }
         }
 
@@ -160,8 +129,7 @@ namespace PosProject_psi
             itemGrid.CurrentRow.Cells[4].Value = prodCount;
             try
             {
-                MessageBox.Show(price.ToString());
-                EventDr.Cells[3].Value = (price * int.Parse(EventDr.Cells[4].Value.ToString())).ToString();
+                itemGrid.CurrentRow.Cells[3].Value = (price * int.Parse(itemGrid.CurrentRow.Cells[4].Value.ToString())).ToString();
             }
             catch (FormatException)
             {
@@ -188,6 +156,7 @@ namespace PosProject_psi
             cmd.CommandType = CommandType.StoredProcedure;
             cmd.Parameters.AddWithValue("@eventNum", eventNum);
             con.Open();
+
             adapter = DbMan.DbAdap(adapter);
             adapter.SelectCommand = cmd;
             ds = DbMan.DbDs(ds);
@@ -203,18 +172,11 @@ namespace PosProject_psi
                 }
                 else
                 {
-                    int bonus = 0;
-                    //EventDr = itemGrid.Rows[itemGrid.RowCount - 1];
-                    bonus = int.Parse(EventDr.Cells[4].Value.ToString()) / (int.Parse(er[0].ToString()) + 1);
-                    //MessageBox.Show(EventDr.Cells[4].Value.ToString());
-                    //MessageBox.Show(bonus.ToString());
-                    //MessageBox.Show(price.ToString());
+                    int bonus = int.Parse(itemGrid.CurrentRow.Cells[4].Value.ToString()) / (int.Parse(er[0].ToString()) + 1);
                     if (bonus >= 1)
                     {
-                        //MessageBox.Show(EventDr.Index.ToString());
-                        EventDr.Cells[3].Value = int.Parse(EventDr.Cells[3].Value.ToString()) - (price * bonus);
+                        itemGrid.CurrentRow.Cells[3].Value = int.Parse(itemGrid.CurrentRow.Cells[3].Value.ToString()) - (price * bonus);
                     }
-                    
                 }
             }
         }
@@ -247,8 +209,6 @@ namespace PosProject_psi
             {
                 if (int.Parse(txtReturnMoney.Text) >= 0)
                 {
-                    SellProdInsert();
-                    ProdMinus();
                     AutoClosingMessageBox.Show("정상처리되었습니다.", "GD편의점", 2000);
                     itemGrid.Rows.Clear();
                     txtBacode.Clear();
@@ -303,7 +263,7 @@ namespace PosProject_psi
                     bacode = er[1].ToString();
                     txtProdInfo.Text += "<상품 명>\r\n";
                     txtProdInfo.Text += er[0] + "\r\n\r\n";
-                    eventPrice = price; 
+                    
                 }
             }
             else
@@ -327,41 +287,21 @@ namespace PosProject_psi
                     txtProdInfo.Text += "<이벤트 내용>\r\n";
                     txtProdInfo.Text += er[6] + "\r\n\r\n";
                     eventNum = int.Parse(er[9].ToString());
-                    eventPrice = price;
                 }
-
+               
             }
-            EventDr = itemGrid.CurrentRow;
             string barcodeNum = txtBacode.Text;
             ProdImage(barcodeNum);
             //DiscountEvent();
+            
             for (int i = 0; i < itemGrid.Rows.Count-1; i++)
             {
                 if(itemGrid.Rows[i].Cells[2].Value.ToString() == bacode)
                 {
-                   
                     itemGrid.Rows[i].Cells[4].Value = int.Parse(itemGrid.Rows[i].Cells[4].Value.ToString()) + 1;
                     itemGrid.Rows[i].Cells[3].Value = (price * int.Parse(itemGrid.Rows[i].Cells[4].Value.ToString())).ToString(); //qq
                     itemGrid.Rows.Remove(itemGrid.Rows[itemGrid.Rows.Count - 1]);
                     noCount--;
-                    itemGrid.Rows[i].Selected = true;
-                }
-                else
-                {
-                    for (int f = 0; f < itemGrid.RowCount; f++)
-                    {
-                        itemGrid.Rows[f].Selected = false;
-                    }
-                    itemGrid.Rows[itemGrid.RowCount-1].Selected = true;
-                }
-            }
-
-
-            for (int f = 0; f < itemGrid.RowCount; f++)
-            {
-                if (itemGrid.Rows[f].Selected == true)
-                {
-                    EventDr = itemGrid.Rows[f];
                 }
             }
             //try
@@ -387,7 +327,7 @@ namespace PosProject_psi
             }
             prodCount = "";
             rows.Clear();
-            //BonusEvent();
+            BonusEvent();
         }
 
         //private void DiscountEvent()
@@ -440,6 +380,7 @@ namespace PosProject_psi
             picAdapter = null;
             pro.Clear();
         }
+
         private void btnCancel_Click(object sender, EventArgs e)
         {
             itemGrid.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
@@ -471,35 +412,6 @@ namespace PosProject_psi
 
         private void itemGrid_CellClick(object sender, DataGridViewCellEventArgs e)
         {
-            using (var conn = DbMan.Dbcon(sqlcon))
-            {
-                conn.Open();
-                using (var cmdd = new SqlCommand("ProCount", conn))
-                {
-                    cmdd.CommandType = CommandType.StoredProcedure;
-                    cmdd.Parameters.AddWithValue("@barcode", itemGrid.CurrentRow.Cells[2].Value.ToString());
-                    adapter = DbMan.DbAdap(adapter);
-                    adapter.SelectCommand = cmdd;
-                    ds = DbMan.DbDs(ds);
-                    adapter.Fill(ds);
-                    DataTable proo = ds.Tables[0];
-                    DataRowCollection rowss = proo.Rows;
-                    foreach (DataRow er in rowss)
-                    {
-                        eventPrice = int.Parse(er[0].ToString());
-                    }
-                }
-            }
-
-
-            for (int i = 0; i < itemGrid.RowCount; i++)
-            {
-                if (itemGrid.Rows[i].Selected == true)
-                {
-                    EventDr = itemGrid.Rows[i];
-                    price = eventPrice;
-                }
-            }
             button1.Enabled = true;
             txtProdInfo.Text = "";
             cursor = null;
@@ -590,8 +502,6 @@ namespace PosProject_psi
             {
                 if (int.Parse(txtReturnMoney.Text) >= 0)
                 {
-                    ProdMinus();
-                    SellProdInsert();
                     AutoClosingMessageBox.Show("정상처리되었습니다.", "GD편의점", 2000);
                     itemGrid.Rows.Clear();
                     txtBacode.Clear();
@@ -600,6 +510,8 @@ namespace PosProject_psi
                     txtProdInfo.Clear();
                     txtReturnMoney.Clear();
                     pictureBox1.Image = null;
+                    ProdMinus();
+                    SellProdInsert();
                 }
                 else
                 {
@@ -610,111 +522,30 @@ namespace PosProject_psi
 
         private void SellProdInsert()
         {
-            
-            for (int i = 0; i < itemGrid.Rows.Count; i++)
+            var con = DbMan.Dbcon(sqlcon);
+            var cmd = new SqlCommand("SellProduct", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            con.Open();
+            for (int i = 0; i < itemGrid.Rows.Count - 1; i++)
             {
-                var con = DbMan.Dbcon(sqlcon);
-                var cmd = new SqlCommand("SellLMoneyInsert", con);
-                cmd.CommandType = CommandType.StoredProcedure;
-                con.Open();
-                cmd.Parameters.AddWithValue("@selldate", DateTime.Now);
+                cmd.Parameters.AddWithValue("@prodcount", int.Parse(itemGrid.Rows[i].Cells[4].Value.ToString()));
                 cmd.Parameters.AddWithValue("@barcode", itemGrid.Rows[i].Cells[2].Value.ToString());
-                cmd.Parameters.AddWithValue("@count", itemGrid.Rows[i].Cells[4].Value.ToString());
-                cmd.Parameters.AddWithValue("@age", lblAge.Text);
-                cmd.Parameters.AddWithValue("@gender", lblGender.Text);
-                cmd.Parameters.AddWithValue("@empnum", 49);
                 cmd.ExecuteNonQuery();
-                con.Close();
             }
         }
 
         private void ProdMinus()
         {
-            
-            for (int i = 0; i < itemGrid.Rows.Count; i++)
+            var con = DbMan.Dbcon(sqlcon);
+            var cmd = new SqlCommand("SellProduct", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            con.Open();
+            for (int i = 0; i < itemGrid.Rows.Count - 1; i++)
             {
-                var con = DbMan.Dbcon(sqlcon);
-                var cmd = new SqlCommand("SellProduct", con);
-                cmd.CommandType = CommandType.StoredProcedure;
-                con.Open();
                 cmd.Parameters.AddWithValue("@prodcount", int.Parse(itemGrid.Rows[i].Cells[4].Value.ToString()));
                 cmd.Parameters.AddWithValue("@barcode", itemGrid.Rows[i].Cells[2].Value.ToString());
                 cmd.ExecuteNonQuery();
             }     
-        }
-
-        private void btnCard_Click(object sender, EventArgs e)
-        {
-            if (txtPrice.Text != "0")
-            {  
-                cp = new CardPay();
-                cp.StartPosition = this.StartPosition;
-                cp.Show();
-                this.Hide();
-                cp.txtMoney.Text = this.txtPrice.Text;
-                cp.btnOK.Click += BtnOK_Click;
-                cp.btnCancel.Click += BtnCancel_Click;
-            }
-        }
-
-        private void BtnCancel_Click(object sender, EventArgs e)
-        {
-            cp.Close();
-            this.Show();
-        }
-
-        private void BtnOK_Click(object sender, EventArgs e)
-        {
-
-            if (cp.txtCardNum.Text == "" || cp.txtYear.Text == "" || cp.txtMonth.Text == "")
-            {
-                MessageBox.Show("카드 정보를 모두 입력해주세요");
-                return;
-            }
-            else if (cp.txtCardCom.Text =="")
-            {
-                MessageBox.Show("카드 정보를 조회해 주세요.");
-            }
-            else
-            {
-                CardPayDB();
-                ProdMinus();
-                itemGrid.Rows.Clear();
-                txtBacode.Clear();
-                txtMoney.Clear();
-                txtPrice.Clear();
-                txtProdInfo.Clear();
-                txtReturnMoney.Clear();
-                pictureBox1.Image = null;
-                cp.Close();
-                this.Show();
-            }           
-        }
-
-        private void CardPayDB()
-        {
-            int j = 0;
-            for (int i = 0; i < itemGrid.Rows.Count; i++)
-            {
-                MessageBox.Show(cp.txtYear.Text + "-" + cp.txtMonth.Text + "-01");
-                var con = DbMan.Dbcon(sqlcon);
-                var cmd = new SqlCommand("SellCardInsert", con);
-                cmd.CommandType = CommandType.StoredProcedure;
-                con.Open();
-                cmd.Parameters.AddWithValue("@selldate", DateTime.Now);
-                cmd.Parameters.AddWithValue("@barcode", itemGrid.Rows[i].Cells[2].Value.ToString());
-                cmd.Parameters.AddWithValue("@count", itemGrid.Rows[i].Cells[4].Value.ToString());
-                cmd.Parameters.AddWithValue("@cardCom", cp.txtCardCom.Text);
-                cmd.Parameters.AddWithValue("@cardDate", DateTime.Parse(cp.txtYear.Text + "-" + cp.txtMonth.Text + "-01"));
-                //DateTime.Parse(cp.txtYear.Text + "-" + cp.txtMonth.Text + "-01")
-                cmd.Parameters.AddWithValue("@cardNum", cp.txtCardNum.Text);
-                cmd.Parameters.AddWithValue("@empnum", 49);
-                cmd.Parameters.AddWithValue("@age", this.lblAge.Text);
-                cmd.Parameters.AddWithValue("@gender", this.lblGender.Text);
-                j = cmd.ExecuteNonQuery();
-            }
-                AutoClosingMessageBox.Show("정상처리되었습니다.", "GD편의점", 2000);
-            
         }
     }
 }
